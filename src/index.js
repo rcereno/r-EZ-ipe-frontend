b16089d57264a5c0db349a65bfe15af2e9170e35
Rhoyalinn C <56568978+rcereno@users.noreply.github.com>
2022-04-10 10:45:03 -0700

    added some category boxes to the home page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Demo from './demo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Demo />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  document.querySelector('root')
 );
 reportWebVitals();
+
