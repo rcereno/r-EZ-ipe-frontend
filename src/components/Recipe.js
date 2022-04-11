b1beb9a932ea4a1ed44285de899179473b2ae614
Eugenia Morales <eumorale@calpoly.edu>
2022-04-11 15:02:03 -0700

    new colors

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
       width: "50%",
       marginLeft:"25%",
       borderRadius: 40,
-      background: 'linear-gradient(#FFFFFF 30%, #D2B48C 150%)',
+      background: 'linear-gradient(#FFFFFF 30%, #C9F5EA 150%)',
       marginTop:"5%",
       boxShadow: "1px 1px 25px #9E9E9E"
         
