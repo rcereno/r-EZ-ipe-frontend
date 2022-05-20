fb51fdbe81f9d06806a1f616e606e3fc35c53a5a
Rhoyalinn C <56568978+rcereno@users.noreply.github.com>
2022-05-20 15:41:41 -0700

    merge pull request for code rev

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,29 +1,96 @@
 import React, { useEffect, useState } from 'react';
 import Sidebar from "./Sidebar";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+const useStyles = makeStyles((theme) => ({
+    
+    root: {
+      width: "50%",
+      marginLeft:"25%",
+      borderRadius: 40,
+      background: 'linear-gradient(#FFFFFF 80%, #B1FAF6 110%)',
+      marginTop:"5%",
+      boxShadow: "1px 1px 25px #9E9E9E"
+        
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }));
 
 function Recipe() {
-    useEffect(() => {
+    const classes = useStyles();
+    const [setAnchorEl] = React.useState(null);
+    const [expanded, setExpanded] = React.useState(false);
+        useEffect(() => {
         fetchItems();
     }, []);
 
     const [items, setItems] = useState([]);
 
     const fetchItems = async () => {
-        const data = await fetch('/recipes');
+        const data = await fetch('https://r-ez-ipe-api.herokuapp.com/recipes');
         const items = await data.json();
         setItems(items);
     };
 
+  
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+
+   
+
     return (
         <body style={{
             background: "linear-gradient(#FFFFFF, #D2B48C)",
-            height: '1000px'
-
+            height: '100%',
+    
         }}>
         <section>
             <div class="recipes">
-                <h1 class="mt-5">Recipes</h1>
-                <h5 class="mt-5"> Browse our recipes or submit your own</h5>
+            <Typography variant="h1" color="textSecondary">
+                                    Recipes
+                                    <Typography variant="body1" color="textPrimary" compone nt="p">
+                                    Browse our recipes or submit your own
+                                </Typography>
+                                </Typography>
                 <form method="POST" action="/addRecipe">
                     <div class="input-group justify-content-center">
                         <div class="input-group-prepend">
@@ -40,37 +107,61 @@ function Recipe() {
                 </form>
                 <Sidebar />
 
-                {
-                    
+               {
                     items.map(item => (
-                        <div class="accordion" id="accordionExample">
-                            <div class="card">
-                                <div class="card-header" id="headingOne">
-                                    <h2 class="mb-0">
-                                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            <i> Recipe for {item.name} </i>
-                                        </button>
-                                    </h2>
-                                </div>
-
-                                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                                    <div class="card-body">
-                                        <img src={item.imageURL} height="150" width="150" />
-                                        <div> <b>Steps:</b> <li>{item.steps}</li> </div>
-
-
-                                        <div> <b>Ingredients:</b> <li>{item.ingredients}</li> </div>
-                                        
-
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+                        <Card className={classes.root}>
+                    
+                        <><CardHeader
+                            avatar={<Avatar aria-label="recipe" className={classes.avatar}>
+                                EZ
+                            </Avatar>}
+                            action={<IconButton >
+                                <MoreVertIcon />
+                            </IconButton>}
+                            title= {item.name}
+                            /><CardMedia
+                                className={classes.media}
+                                image={item.imageURL}
+                                title="Ingredients" /><CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Ingredients needed: {item.ingredients}
+                                </Typography>
+                            </CardContent><CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share" href={item.originalURL} target="_blank" rel="noopener noreferrer"  onClick = {handleClick}>
+                                {/* <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClick={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Source</MenuItem>
+        </Menu> */}
+                                    <ShareIcon />
+                                </IconButton>
+                                <IconButton
+                                    className={clsx(classes.expand, {
+                                        [classes.expandOpen]: expanded,
+                                    })}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>
+                            </CardActions><Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <Typography paragraph>Method:</Typography>
+                                    <Typography paragraph>
+                                       {item.steps}
+                                    </Typography>
+                                </CardContent>
+                            </Collapse></>
+                            </Card>
                     ))
-                }
+                                }
             </div>
         </section>
         </body>
