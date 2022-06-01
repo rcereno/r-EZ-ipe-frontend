7f3d3e9cf015500192ee2a015431f5782a2b504c
Eugenia Morales <eumorale@calpoly.edu>
2022-06-01 12:04:18 -0700

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Select recipes to browse our collection or to input your own!/i);
   expect(linkElement).toBeInTheDocument();
 });
