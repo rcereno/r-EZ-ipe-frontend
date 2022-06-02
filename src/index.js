7d9fb2fbdd634b09807124714060e893273f67aa
niknagarajan <91632385+niknagarajan@users.noreply.github.com>
2022-06-02 16:22:26 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Boxes from './components/Boxes';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 reportWebVitals();
+
