4792aba6322aaf9a774591a800e3f670d8c5fcb4
Rhoyalinn C <56568978+rcereno@users.noreply.github.com>
2022-06-07 13:23:40 -0700

    updated fetch link

--- src/pages/Keto.js
+++ src/pages/Keto.js
@@ -55,7 +55,7 @@ export const Keto = () => {
   const [items, setItems] = useState([]);
 
   const fetchItems = async () => {
-    const data = await fetch("https://r-ez-ipe-api.herokuapp.com/ketorecipes");
+    const data = await fetch("https://r-ez-ipe.herokuapp.com/ketorecipes");
     const items = await data.json();
     setItems(items);
   };
